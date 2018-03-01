af9268be15dd33d0530e2f3eeb8d3fb16e989f27
Antonio Rodríguez Artacho <antonio.rdgz.a@gmail.com>
2018-03-01 18:45:36 +0100

    added classes Manager, CreditCard and Request

---
+++ Acme-Rendezvous/src/main/java/domain/CreditCard.java
@@ -0,0 +1,80 @@
+
+package domain;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+
+import org.hibernate.validator.constraints.CreditCardNumber;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.Range;
+
+@Entity
+@Access(AccessType.PROPERTY)
+public class CreditCard extends DomainEntity {
+
+	// Constructors -----------------------------------------------------------
+
+	// Attributes -------------------------------------------------------------
+	private String	holderName;
+	private String	brandName;
+	private String	number;
+	private Integer	expirationMonth;
+	private Integer	expirationYear;
+	private Integer	cvv;
+
+
+	@NotBlank
+	public String getHolderName() {
+		return this.holderName;
+	}
+
+	public void setHolderName(final String holderName) {
+		this.holderName = holderName;
+	}
+
+	@NotBlank
+	public String getBrandName() {
+		return this.brandName;
+	}
+
+	public void setBrandName(final String brandName) {
+		this.brandName = brandName;
+	}
+	@NotBlank
+	@CreditCardNumber
+	public String getNumber() {
+		return this.number;
+	}
+
+	public void setNumber(final String number) {
+		this.number = number;
+	}
+	@Range(min = 1, max = 12)
+	public Integer getExpirationMonth() {
+		return this.expirationMonth;
+	}
+
+	public void setExpirationMonth(final Integer expirationMonth) {
+		this.expirationMonth = expirationMonth;
+	}
+
+	@Range(min = 00, max = 99)
+	public Integer getExpirationYear() {
+		return this.expirationYear;
+	}
+
+	public void setExpirationYear(final Integer expirationYear) {
+		this.expirationYear = expirationYear;
+	}
+
+	@Range(min = 100, max = 999)
+	public Integer getCvv() {
+		return this.cvv;
+	}
+
+	public void setCvv(final Integer cvv) {
+		this.cvv = cvv;
+	}
+	// Relationships ----------------------------------------------------------
+}
