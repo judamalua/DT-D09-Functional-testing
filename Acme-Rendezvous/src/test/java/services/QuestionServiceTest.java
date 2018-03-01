010e06087e4f5522e2670201315d06dbceb2f7ba
Juan Miguel <juanmiblancof@gmail.com>
2018-03-01 19:19:18 +0100

    Fixed Unit bug

---
+++ Acme-Rendezvous/src/test/java/services/QuestionServiceTest.java
@@ -0,0 +1,21 @@
+
+package services;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+	"classpath:spring/junit.xml"
+})
+@Transactional
+public class QuestionServiceTest {
+
+	// The SUT ---------------------------------------------------------------
+	private QuestionService	questionService;
+
+	// Tests ------------------------------------------------------------------
+
+}
