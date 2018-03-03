53cdb853b842ffdf09418f07ce4fc93508bda4d5
Juan Miguel <juanmiblancof@gmail.com>
2018-03-03 21:01:20 +0100

    Added a deleted rendezvous to populate, implemented several test cases into rendezvous tests

--- Acme-Rendezvous/src/test/java/services/RendezvousServiceTest.java
+++ Acme-Rendezvous/src/test/java/services/RendezvousServiceTest.java
@@ -55,13 +55,86 @@ public class RendezvousServiceTest extends AbstractTest {
 		final Object testingData[][] = {
 			{
 				// This test checks that authenticated users can create a rendezvous
-				"User1", "Prueba", "Prueba", new Date(System.currentTimeMillis() + oneDay), "https://cdns3.eltiempo.es/eltiempo/blog/noticias/2015/07/olas1.jpg", "123.12,123.12", false, false, null
+				"User1", "Test", "Test", new Date(System.currentTimeMillis() + oneDay), "https://cdns3.eltiempo.es/eltiempo/blog/noticias/2015/07/olas1.jpg", "123.12,123.12", false, false, null
+			}, {
+				// This test checks that authenticated users can create a rendezvous with an empty photo URL
+				"User1", "Test", "Test", new Date(System.currentTimeMillis() + oneDay), "", "123.12,123.12", false, false, null
+			}, {
+				// This test checks that authenticated users can create a rendezvous in final mode
+				"User1", "Test", "Test", new Date(System.currentTimeMillis() + oneDay), "", "123.12,123.12", true, false, null
+			}, {
+				// This test checks that authenticated users can create a rendezvous as adult only
+				"User1", "Test", "Test", new Date(System.currentTimeMillis() + oneDay), "", "123.12,123.12", false, true, null
+			}, {
+				// This test checks that authenticated users can create a rendezvous in final mode and as adult only
+				"User1", "Test", "Test", new Date(System.currentTimeMillis() + oneDay), "", "123.12,123.12", true, true, null
+			}, {
+				// This test checks that unauthenticated users cannot create a rendezvous
+				null, "Test", "Test", new Date(System.currentTimeMillis() + oneDay), "https://cdns3.eltiempo.es/eltiempo/blog/noticias/2015/07/olas1.jpg", "123.12,123.12", false, false, IllegalArgumentException.class
+			}, {
+				// This test checks that actors not logged as users cannot create a rendezvous
+				"Admin1", "Test", "Test", new Date(System.currentTimeMillis() + oneDay), "https://cdns3.eltiempo.es/eltiempo/blog/noticias/2015/07/olas1.jpg", "123.12,123.12", false, false, ClassCastException.class
+			}, {
+				// This test checks that a rendezvous with empty name cannot be created
+				"User1", "", "Test", new Date(System.currentTimeMillis() + oneDay), "https://cdns3.eltiempo.es/eltiempo/blog/noticias/2015/07/olas1.jpg", "123.12,123.12", false, false, javax.validation.ConstraintViolationException.class
+			}, {
+				// This test checks that a rendezvous with empty description cannot be created
+				"User1", "Test", "", new Date(System.currentTimeMillis() + oneDay), "https://cdns3.eltiempo.es/eltiempo/blog/noticias/2015/07/olas1.jpg", "123.12,123.12", false, false, javax.validation.ConstraintViolationException.class
+			}, {
+				// This test checks that a rendezvous with null moment cannot be created
+				"User1", "Test", "", null, "https://cdns3.eltiempo.es/eltiempo/blog/noticias/2015/07/olas1.jpg", "123.12,123.12", false, false, javax.validation.ConstraintViolationException.class
+			}, {
+				// This test checks that a rendezvous with past moment cannot be created
+				"User1", "Test", "", new Date(System.currentTimeMillis() - oneDay), "https://cdns3.eltiempo.es/eltiempo/blog/noticias/2015/07/olas1.jpg", "123.12,123.12", false, false, javax.validation.ConstraintViolationException.class
+			}, {
+				// This test checks that a rendezvous with empty GPS Coordinates cannot be created
+				"User1", "Test", "", new Date(System.currentTimeMillis() + oneDay), "https://cdns3.eltiempo.es/eltiempo/blog/noticias/2015/07/olas1.jpg", "", false, false, javax.validation.ConstraintViolationException.class
 			}
 		};
 
 		for (int i = 0; i < testingData.length; i++)
 			this.templateCreate((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2], (Date) testingData[i][3], (String) testingData[i][4], (String) testingData[i][5], (Boolean) testingData[i][6], (Boolean) testingData[i][7],
 				(Class<?>) testingData[i][8]);
+	}
+
+	/**
+	 * This driver checks several tests regarding functional requirement number 5.2: An actor who is authenticated as a user must be able to create a rendezvous, which he’s implicitly assumed to attend.
+	 * Note that a user may edit his or her rendezvouses as long as they aren’t saved them in final mode.
+	 * Once a rendezvous is saved in final mode, it cannot be edited or deleted by the creator
+	 * 
+	 * 5.3: An actor who is authenticated as a user must be able to update or delete the rendezvouses that he or she’s created. Deletion is virtual, that
+	 * is: the information is not removed from the database, but the rendezvous cannot be updated. Deleted rendezvouses are flagged as such when they are displayed.
+	 * 
+	 * 5.4: An actor who is authenticated as a user must be able to RSVP a rendezvous or cancel it. When a user RSVPs a rendezvous, he or she is assumed to attend it.
+	 * 
+	 * @author Juanmi
+	 */
+	@Test
+	public void driverEditRendezvous() {
+		final Long oneDay = TimeUnit.DAYS.toMillis(1);
+		final Object testingData[][] = {
+			{
+				// This test checks that authenticated users can edit a draft mode rendezvous they created
+				"User1", "Rendezvous8", "Test", "Test", new Date(System.currentTimeMillis() + oneDay), "https://cdns3.eltiempo.es/eltiempo/blog/noticias/2015/07/olas1.jpg", "123.12,123.12", false, false, null
+			},
+			{
+				// This test checks that authenticated users cannot edit a rendezvous they did not create
+				"User2", "Rendezvous8", "Test", "Test", new Date(System.currentTimeMillis() + oneDay), "https://cdns3.eltiempo.es/eltiempo/blog/noticias/2015/07/olas1.jpg", "123.12,123.12", false, false,
+				org.springframework.dao.DataIntegrityViolationException.class
+			},
+			{
+				// This test checks that authenticated actors that are not users cannot edit a draft mode rendezvous they created
+				"Admin1", "Rendezvous8", "Test", "Test", new Date(System.currentTimeMillis() + oneDay), "https://cdns3.eltiempo.es/eltiempo/blog/noticias/2015/07/olas1.jpg", "123.12,123.12", false, false,
+				org.springframework.dao.DataIntegrityViolationException.class
+			}, {
+				// This test checks that unauthenticated users cannot edit a rendezvous
+				null, "Rendezvous8", "Test", "Test", new Date(System.currentTimeMillis() + oneDay), "https://cdns3.eltiempo.es/eltiempo/blog/noticias/2015/07/olas1.jpg", "123.12,123.12", false, false, IllegalArgumentException.class
+			}
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.templateEdit((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2], (String) testingData[i][3], (Date) testingData[i][4], (String) testingData[i][5], (String) testingData[i][6], (Boolean) testingData[i][7],
+				(Boolean) testingData[i][8], (Class<?>) testingData[i][9]);
 	}
 
 	// Ancillary methods ------------------------------------------------------
@@ -124,4 +197,38 @@ public class RendezvousServiceTest extends AbstractTest {
 		this.checkExceptions(expected, caught);
 	}
 
+	protected void templateEdit(final String username, final String rendezvousPopulateName, final String name, final String description, final Date moment, final String pictureUrl, final String gpsCoordinates, final boolean finalMode,
+		final boolean adultOnly, final Class<?> expected) {
+		Class<?> caught;
+		int rendezvousId;
+		final Rendezvous rendezvous;
+
+		caught = null;
+
+		try {
+			super.authenticate(username);
+
+			rendezvousId = super.getEntityId(rendezvousPopulateName);
+
+			rendezvous = this.rendezvousService.findOne(rendezvousId);
+
+			rendezvous.setName(name);
+			rendezvous.setDescription(description);
+			rendezvous.setMoment(moment);
+			rendezvous.setPictureUrl(pictureUrl);
+			rendezvous.setGpsCoordinates(gpsCoordinates);
+			rendezvous.setFinalMode(finalMode);
+			rendezvous.setAdultOnly(adultOnly);
+
+			this.rendezvousService.save(rendezvous);
+			this.rendezvousService.flush();
+
+			super.unauthenticate();
+
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		}
+
+		this.checkExceptions(expected, caught);
+	}
 }
