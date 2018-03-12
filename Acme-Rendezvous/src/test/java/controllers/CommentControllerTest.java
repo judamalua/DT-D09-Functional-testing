368f9ddc9c2768b54e9093696607520d432b1860
Antonio Rodríguez Artacho <antonio.rdgz.a@gmail.com>
2018-03-12 13:56:08 +0100

    Test comment controller finished

--- Acme-Rendezvous/src/test/java/controllers/CommentControllerTest.java
+++ Acme-Rendezvous/src/test/java/controllers/CommentControllerTest.java
@@ -24,7 +24,6 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import services.CommentService;
 import services.ConfigurationService;
-import services.RendezvousService;
 import utilities.AbstractTest;
 import controllers.user.CommentUserController;
 import domain.Comment;
@@ -48,10 +47,6 @@ public class CommentControllerTest extends AbstractTest {
 	@Mock
 	@Autowired
 	private CommentService			service;
-
-	@Mock
-	@Autowired
-	private RendezvousService		rendezvousService;
 
 	@Mock
 	@Autowired
@@ -96,6 +91,7 @@ public class CommentControllerTest extends AbstractTest {
 			.andExpect(MockMvcResultMatchers.model().attribute("rendezvous", Matchers.hasProperty("id", Matchers.is(rendezvousId))))
 			.andExpect(MockMvcResultMatchers.model().attribute("requestURI", Matchers.is("comment/user/edit.do?rendezvousId=" + rendezvousId)));
 
+		super.unauthenticate();
 	}
 
 	/**
@@ -143,8 +139,80 @@ public class CommentControllerTest extends AbstractTest {
 
 		this.mockMvc.perform(request).andExpect(MockMvcResultMatchers.status().is(302)).andExpect(MockMvcResultMatchers.view().name("redirect:/misc/403")).andExpect(MockMvcResultMatchers.forwardedUrl(null));
 
+		super.unauthenticate();
 	}
 
+	/**
+	 * Test the create comment POST for a user who hasn't RSVPd the Rendezvous. Must return 403 code.
+	 * Functional requirement:
+	 * 5.6) Comment on the rendezvouses that he or she has RSVPd.
+	 * 
+	 * @throws Throwable
+	 * @author Antonio
+	 */
+	@Test
+	public void testSaveCommentToNotRSVPdRendezvousNegative() throws Throwable {
+		final MockHttpServletRequestBuilder request;
+		int rendezvousId;
+
+		super.authenticate("user5");
+		rendezvousId = super.getEntityId("Rendezvous4"); //Table games
+
+		request = MockMvcRequestBuilders.post("/comment/user/edit.do?rendezvousId=" + rendezvousId).contentType(MediaType.APPLICATION_FORM_URLENCODED).param("text", "Comment Test text").param("pictureUrl", "http://majoumo.com/images/picture/picture2.jpg")
+			.param("moment", "03/03/2018 00:15").sessionAttr("comment", new Comment()).param("save", "");
+
+		this.mockMvc.perform(request).andExpect(MockMvcResultMatchers.status().is(302)).andExpect(MockMvcResultMatchers.view().name("redirect:/misc/403"));
+
+		super.unauthenticate();
+	}
+
+	/**
+	 * Test the create comment POST for a user who has RSVPd the Rendezvous. The comment doesn't have text. Must return 200 code, to the edit comment view.
+	 * Functional requirement:
+	 * 5.6) Comment on the rendezvouses that he or she has RSVPd.
+	 * 
+	 * @throws Throwable
+	 * @author Antonio
+	 */
+	@Test
+	public void testSaveCommentWithoutTextToRendezvousNegative() throws Throwable {
+		final MockHttpServletRequestBuilder request;
+		int rendezvousId;
+
+		super.authenticate("user1");
+		rendezvousId = super.getEntityId("Rendezvous4"); //Table games
+
+		request = MockMvcRequestBuilders.post("/comment/user/edit.do?rendezvousId=" + rendezvousId).contentType(MediaType.APPLICATION_FORM_URLENCODED).param("text", "").param("pictureUrl", "http://majoumo.com/images/picture/picture2.jpg")
+			.param("moment", "03/03/2018 00:15").sessionAttr("comment", new Comment()).param("save", "");
+
+		this.mockMvc.perform(request).andExpect(MockMvcResultMatchers.status().is(200)).andExpect(MockMvcResultMatchers.view().name("comment/edit")).andExpect(MockMvcResultMatchers.forwardedUrl("comment/edit"));
+
+		super.unauthenticate();
+	}
+
+	/**
+	 * Test the create comment POST for a user who has RSVPd the Rendezvous. The comment has an invalid URL in the pictureUrl parameter. Must return 200 code, to the edit comment view.
+	 * Functional requirement:
+	 * 5.6) Comment on the rendezvouses that he or she has RSVPd.
+	 * 
+	 * @throws Throwable
+	 * @author Antonio
+	 */
+	@Test
+	public void testSaveCommentWithoutValidPictureUrlToRendezvousNegative() throws Throwable {
+		final MockHttpServletRequestBuilder request;
+		int rendezvousId;
+
+		super.authenticate("user1");
+		rendezvousId = super.getEntityId("Rendezvous4"); //Table games
+
+		request = MockMvcRequestBuilders.post("/comment/user/edit.do?rendezvousId=" + rendezvousId).contentType(MediaType.APPLICATION_FORM_URLENCODED).param("text", "Comment test text").param("pictureUrl", "this is an invalid url")
+			.param("moment", "03/03/2018 00:15").sessionAttr("comment", new Comment()).param("save", "");
+
+		this.mockMvc.perform(request).andExpect(MockMvcResultMatchers.status().is(200)).andExpect(MockMvcResultMatchers.view().name("comment/edit")).andExpect(MockMvcResultMatchers.forwardedUrl("comment/edit"));
+
+		super.unauthenticate();
+	}
 	//-----------------------REPLY COMMENT TESTS------------------------------
 
 	/**
@@ -173,6 +241,7 @@ public class CommentControllerTest extends AbstractTest {
 			.andExpect(MockMvcResultMatchers.model().attribute("requestURI", Matchers.is("comment/user/reply.do?repliedId=" + repliedCommentId)))
 			.andExpect(MockMvcResultMatchers.model().attribute("rendezvous", Matchers.hasProperty("id", Matchers.is(rendezvousId))));
 
+		super.unauthenticate();
 	}
 
 	/**
@@ -196,5 +265,55 @@ public class CommentControllerTest extends AbstractTest {
 
 		this.mockMvc.perform(request).andExpect(MockMvcResultMatchers.status().is(302)).andExpect(MockMvcResultMatchers.view().name("redirect:/misc/403")).andExpect(MockMvcResultMatchers.forwardedUrl(null));
 
+		super.unauthenticate();
+
+	}
+
+	/**
+	 * Test the reply comment POST for a user who has RSVPd the Rendezvous. The reply comment has a blank text. Must return 200 code, to the edit comment view.
+	 * Information requirement;
+	 * 19) In addition to writing a comment from scratch, a user may reply to a comment.
+	 * 
+	 * @throws Throwable
+	 * @author Antonio
+	 */
+	@Test
+	public void testReplyCommentWithoutTextNegative() throws Throwable {
+		final MockHttpServletRequestBuilder request;
+		int repliedCommentId;
+
+		super.authenticate("user1");
+		repliedCommentId = super.getEntityId("Comment8"); //Replied comment
+
+		request = MockMvcRequestBuilders.post("/comment/user/reply.do?repliedId=" + repliedCommentId).contentType(MediaType.APPLICATION_FORM_URLENCODED).param("text", "").param("pictureUrl", "http://majoumo.com/images/picture/picture2.jpg")
+			.param("moment", "03/03/2018 00:15").sessionAttr("comment", new Comment()).param("save", "");
+
+		this.mockMvc.perform(request).andExpect(MockMvcResultMatchers.status().is(200)).andExpect(MockMvcResultMatchers.view().name("comment/edit")).andExpect(MockMvcResultMatchers.forwardedUrl("comment/edit"));
+
+		super.unauthenticate();
+	}
+
+	/**
+	 * Test the reply comment POST for a user who has RSVPd the Rendezvous. The reply comment has an invalid URL in the pictureUrl parameter. Must return 200 code, to the edit comment view.
+	 * Information requirement;
+	 * 19) In addition to writing a comment from scratch, a user may reply to a comment.
+	 * 
+	 * @throws Throwable
+	 * @author Antonio
+	 */
+	@Test
+	public void testReplyCommentWithoutValidPictureUrlNegative() throws Throwable {
+		final MockHttpServletRequestBuilder request;
+		int repliedCommentId;
+
+		super.authenticate("user1");
+		repliedCommentId = super.getEntityId("Comment8"); //Replied comment
+
+		request = MockMvcRequestBuilders.post("/comment/user/reply.do?repliedId=" + repliedCommentId).contentType(MediaType.APPLICATION_FORM_URLENCODED).param("text", "Comment test text").param("pictureUrl", "this is an invalid url")
+			.param("moment", "03/03/2018 00:15").sessionAttr("comment", new Comment()).param("save", "");
+
+		this.mockMvc.perform(request).andExpect(MockMvcResultMatchers.status().is(200)).andExpect(MockMvcResultMatchers.view().name("comment/edit")).andExpect(MockMvcResultMatchers.forwardedUrl("comment/edit"));
+
+		super.unauthenticate();
 	}
 }
