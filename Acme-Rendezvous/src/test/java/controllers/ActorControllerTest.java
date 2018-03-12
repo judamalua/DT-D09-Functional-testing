5457b45b6f4412106209981e59b2bbe2f77e09f3
Antonio Rodríguez Artacho <antonio.rdgz.a@gmail.com>
2018-03-12 17:10:14 +0100

    actor controller test

--- Acme-Rendezvous/src/test/java/controllers/ActorControllerTest.java
+++ Acme-Rendezvous/src/test/java/controllers/ActorControllerTest.java
@@ -66,25 +66,72 @@ public class ActorControllerTest extends AbstractTest {
 	//-------------------------- DISPLAY ACTOR TESTS ------------------------------
 
 	/**
-	 * Test the display view of an Actor who is the principal. Must return 200 code.
+	 * Test the display view of an Actor who is the principal, logged as an User. Must return 200 code.
 	 * 
 	 * @throws Throwable
 	 * @author Antonio
 	 */
 	@Test
-	public void testDisplayActorPositive() throws Throwable {
+	public void testDisplayActorUserPositive() throws Throwable {
 		final MockHttpServletRequestBuilder request;
+		int actorId;
 
+		actorId = super.getEntityId("User1");
 		super.authenticate("user1"); //The principal
 
 		request = MockMvcRequestBuilders.get("/actor/display.do");
 
 		this.mockMvc.perform(request).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.view().name("actor/display")).andExpect(MockMvcResultMatchers.forwardedUrl("actor/display"))
-			.andExpect(MockMvcResultMatchers.model().attribute("actor", Matchers.hasProperty("id", Matchers.not(0))));
+			.andExpect(MockMvcResultMatchers.model().attribute("actor", Matchers.hasProperty("id", Matchers.is(actorId))));
 
 		super.unauthenticate();
 	}
 
+	/**
+	 * Test the display view of an Actor who is the principal, logged as an Admin. Must return 200 code.
+	 * 
+	 * @throws Throwable
+	 * @author Antonio
+	 */
+	@Test
+	public void testDisplayActorAdminPositive() throws Throwable {
+		final MockHttpServletRequestBuilder request;
+		int actorId;
+
+		actorId = super.getEntityId("Administrator1");
+
+		super.authenticate("admin"); //The principal
+
+		request = MockMvcRequestBuilders.get("/actor/display.do");
+
+		this.mockMvc.perform(request).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.view().name("actor/display")).andExpect(MockMvcResultMatchers.forwardedUrl("actor/display"))
+			.andExpect(MockMvcResultMatchers.model().attribute("actor", Matchers.hasProperty("id", Matchers.is(actorId))));
+
+		super.unauthenticate();
+	}
+
+	/**
+	 * Test the display view of an Actor who is the principal, logged as a Manager. Must return 200 code.
+	 * 
+	 * @throws Throwable
+	 * @author Antonio
+	 */
+	@Test
+	public void testDisplayActorManagerPositive() throws Throwable {
+		final MockHttpServletRequestBuilder request;
+		int actorId;
+
+		actorId = super.getEntityId("Manager1");
+
+		super.authenticate("manager1"); //The principal
+
+		request = MockMvcRequestBuilders.get("/actor/display.do");
+
+		this.mockMvc.perform(request).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.view().name("actor/display")).andExpect(MockMvcResultMatchers.forwardedUrl("actor/display"))
+			.andExpect(MockMvcResultMatchers.model().attribute("actor", Matchers.hasProperty("id", Matchers.is(actorId))));
+
+		super.unauthenticate();
+	}
 	/**
 	 * Test the display view of an Actor who is the principal, but nobody is logged. Must return 302 code and redirect to the misc/403 view.
 	 * 
@@ -98,6 +145,85 @@ public class ActorControllerTest extends AbstractTest {
 		super.authenticate(null); //Not logged
 
 		request = MockMvcRequestBuilders.get("/actor/display.do");
+
+		this.mockMvc.perform(request).andExpect(MockMvcResultMatchers.status().is(302)).andExpect(MockMvcResultMatchers.view().name("redirect:/misc/403"));
+
+		super.unauthenticate();
+	}
+
+	//-------------------------- REGISTER USER TESTS ------------------------------
+	/**
+	 * Test the view to register a new User to the System. Must return 200 code.
+	 * 
+	 * @throws Throwable
+	 * @author Antonio
+	 */
+	@Test
+	public void testRegisterUserGETPositive() throws Throwable {
+		final MockHttpServletRequestBuilder request;
+
+		super.authenticate(null); //Not logged
+
+		request = MockMvcRequestBuilders.get("/actor/register.do");
+
+		this.mockMvc.perform(request).andExpect(MockMvcResultMatchers.status().is(200)).andExpect(MockMvcResultMatchers.view().name("user/register")).andExpect(MockMvcResultMatchers.forwardedUrl("user/register"))
+			.andExpect(MockMvcResultMatchers.model().attribute("actionURL", Matchers.is("actor/register.do"))).andExpect(MockMvcResultMatchers.model().attribute("actor", Matchers.hasProperty("id", Matchers.is(0))))
+			.andExpect(MockMvcResultMatchers.model().attribute("message", Matchers.isEmptyOrNullString()));
+
+		super.unauthenticate();
+	}
+
+	/**
+	 * Test the view to register a new User to the System, when already logged as an User. Must return 403 code.
+	 * 
+	 * @throws Throwable
+	 * @author Antonio
+	 */
+	@Test
+	public void testRegisterUserWhenLoggedAsUserGETNegative() throws Throwable {
+		final MockHttpServletRequestBuilder request;
+
+		super.authenticate("user1");
+
+		request = MockMvcRequestBuilders.get("/actor/register.do");
+
+		this.mockMvc.perform(request).andExpect(MockMvcResultMatchers.status().is(302)).andExpect(MockMvcResultMatchers.view().name("redirect:/misc/403"));
+
+		super.unauthenticate();
+	}
+
+	/**
+	 * Test the view to register a new User to the System, when already logged as a Manager. Must return 403 code.
+	 * 
+	 * @throws Throwable
+	 * @author Antonio
+	 */
+	@Test
+	public void testRegisterUserWhenLoggedAsManagerGETNegative() throws Throwable {
+		final MockHttpServletRequestBuilder request;
+
+		super.authenticate("manager1");
+
+		request = MockMvcRequestBuilders.get("/actor/register.do");
+
+		this.mockMvc.perform(request).andExpect(MockMvcResultMatchers.status().is(302)).andExpect(MockMvcResultMatchers.view().name("redirect:/misc/403"));
+
+		super.unauthenticate();
+	}
+
+	/**
+	 * Test the view to register a new User to the System, when already logged as a Manager. Must return 403 code.
+	 * 
+	 * @throws Throwable
+	 * @author Antonio
+	 */
+	@Test
+	public void testRegisterUserWhenLoggedAsAdminGETNegative() throws Throwable {
+		final MockHttpServletRequestBuilder request;
+
+		super.authenticate("admin");
+
+		request = MockMvcRequestBuilders.get("/actor/register.do");
 
 		this.mockMvc.perform(request).andExpect(MockMvcResultMatchers.status().is(302)).andExpect(MockMvcResultMatchers.view().name("redirect:/misc/403"));
 
