6b75696e6683baef1b489a057c788b61c2bffbca
AlexGue <34453017+AlexGue@users.noreply.github.com>
2018-03-14 03:44:14 +0100

    Update AdministratorServiceTest.java

--- Acme-Rendezvous/src/test/java/services/AdministratorServiceTest.java
+++ Acme-Rendezvous/src/test/java/services/AdministratorServiceTest.java
@@ -6,10 +6,14 @@ import java.util.Date;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+
+import security.Authority;
+import security.UserAccount;
 import utilities.AbstractTest;
 import domain.Administrator;
 
@@ -18,57 +22,65 @@ import domain.Administrator;
 	"classpath:spring/junit.xml"
 })
 @Transactional
-public class AdministratorServiceTest extends AbstractTest {
+public class AdminServiceTest extends AbstractTest {
 
 	// The SUT ---------------------------------------------------------------
 	@Autowired
-	private AdministratorService	adminService;
+	private AdministratorService		adminService;
 
 
 	// Tests ------------------------------------------------------------------
 
 	@Test
-	public void driverCreateAdmin() {
-
+	public void driverCreateAdmin(){
+		
 		//TODO: Preguntar sobre la modificacion de announcement create/delete
 		final Object testingData[][] = {
-			{
-				"Admin1", "TestName", "TestSurname Test", new Date(System.currentTimeMillis() - 1), "testEmail@gmail.com", "655555555", "Test Address", null
-			}, {
-				"Admin1", "", "TestSurname Test", new Date(System.currentTimeMillis() - 1), "testEmail@gmail.com", "655555555", "Test Address", IllegalArgumentException.class
-			}, {
-				null, "TestName", "TestSurname Test", new Date(System.currentTimeMillis() - 1), "testEmail@gmail.com", "655555555", "Test Address", IllegalArgumentException.class
-			}, {
-				"User1", "TestName", "TestSurname Test", new Date(System.currentTimeMillis() - 1), "testEmail@gmail.com", "655555555", "Test Address", IllegalArgumentException.class
-			},
+				{"Admin1", "AdminUser", "TestName", "TestSurname Test", new Date(System.currentTimeMillis()-1), "testEmail@gmail.com","655555555", "Test Address", null},
+				{"Admin1", "AdminUser", "", "TestSurname Test", new Date(System.currentTimeMillis()-1), "testEmail@gmail.com","655555555", "Test Address", DataIntegrityViolationException.class},
+				{null, "AdminUser", "TestName", "TestSurname Test", new Date(System.currentTimeMillis()-1), "testEmail@gmail.com","655555555", "Test Address", IllegalArgumentException.class},
+				{"User1", "AdminUser","TestName", "TestSurname Test", new Date(System.currentTimeMillis()-1), "testEmail@gmail.com","655555555", "Test Address", DataIntegrityViolationException.class},
+				{"Admin1", "", "TestName", "TestSurname Test", new Date(System.currentTimeMillis()-1), "testEmail@gmail.com","655555555", "Test Address", DataIntegrityViolationException.class},
+
+				
 
 		};
-		for (int i = 0; i < testingData.length; i++) {
+		for (int i = 0; i < testingData.length; i++){
 			System.out.println(i);
-			this.templateCreateAdmin((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2], (Date) testingData[i][3], (String) testingData[i][4], (String) testingData[i][5], (String) testingData[i][6],
-				(Class<?>) testingData[i][7]);
-
+			this.templateCreateAdmin((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2], (String) testingData[i][3], (Date) testingData[i][4], (String) testingData[i][5], (String) testingData[i][6], (String) testingData[i][7], (Class<?>)  testingData[i][8]);
+		
 		}
 	}
-
+	
+	
+	
 	/**
-	 * This test checks that authenticated users can answer to questions
-	 * as said in functional requirement 21.2: An actor who is authenticated as
-	 * a user must be able to answer the questions that are associated with a rendezvous
-	 * that he or she's RSVP-ing now.
+	 * This test checks that authenticated admins can create other admins
 	 * 
 	 * @author Alejandro
 	 */
 
-	public void templateCreateAdmin(final String login, final String name, final String surname, final Date birthDate, final String email, final String phoneNumber, final String address, final Class<?> expected) {
-		// Functional requirement number 21.2: An actor who is authenticated as a user must be able to Answer the questions that are associated
-		// with a rendezvous that he or she’s RSVPing now.
+	public void templateCreateAdmin(String login,String userAdmin, String name, String surname, Date birthDate, String email, String phoneNumber, String address, Class<?> expected) {
+		Class<?> caught;
+		caught = null;
+
+		try {
 		Administrator newAdmin;
 
+		
 		super.authenticate(login);
+		
 
 		newAdmin = this.adminService.create();
 		//EN PROGRESO, Falta crear la useraccount
+		UserAccount usAcc = new UserAccount();
+		usAcc.setUsername(userAdmin);
+		usAcc.setPassword("admin");
+		Authority auth = new Authority();
+		auth.setAuthority("ADMIN");
+		usAcc.addAuthority(auth);
+		
+		newAdmin.setUserAccount(usAcc);
 
 		newAdmin.setName(name);
 		newAdmin.setSurname(surname);
@@ -79,8 +91,15 @@ public class AdministratorServiceTest extends AbstractTest {
 
 		this.adminService.save(newAdmin);
 		this.adminService.flush();
-
+		
+		
 		super.unauthenticate();
+		
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		}
+
+		this.checkExceptions(expected, caught);
 	}
 
 }
